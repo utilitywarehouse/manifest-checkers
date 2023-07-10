1855e8052347e99941549411a3b44ac31bcf9aa5
Matt Hughes <mhughes@uw.co.uk>
2023-07-10 14:19:35 +0100

    (devena-mono) OL annotations, handle Base64 encoded manifests
    `validate-opslevel-annotations` is only meant to be used with
    `kustomize-build-dirs`, which writes the built manifests to base64
    encoded filenames, so make sure it can handle these.
    
    Ticket: DENA-133

--- validate-opslevel-annotations/main_test.go
+++ validate-opslevel-annotations/main_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -25,16 +27,47 @@ func init() {
 	testdataDir = filepath.Join(filepath.Dir(file), "testdata")
 }
 
+func TestFailsOnNonBase64Path(t *testing.T) {
+	manifestPath := filepath.Join(t.TempDir(), "not-base-64.yaml")
+	_, err := os.Create(manifestPath)
+	require.NoError(t, err)
+	expectedErrPrefix := "expected a base64 encoded filename at " + manifestPath + ", but failed to decode: "
+
+	err = validateOpsLevelAnnotationsForManifests([]string{manifestPath})
+
+	require.ErrorContains(t, err, expectedErrPrefix)
+}
+
 func TestFailsOnUnreadableFile(t *testing.T) {
-	unreadableFile := filepath.Join(t.TempDir(), "unreadable")
+	decodedPath := "unreadable"
+	unreadableFile := filepath.Join(t.TempDir(), base64.StdEncoding.EncodeToString([]byte(decodedPath)))
 	f, err := os.Create(unreadableFile)
 	require.NoError(t, err)
 	require.NoError(t, f.Chmod(0o200))
-	expectedErrPrefix := "Failed opening manifest: " + unreadableFile
+	expectedErrPrefix := "Failed opening manifest: " + decodedPath
 
 	err = validateOpsLevelAnnotationsForManifests([]string{unreadableFile})
 
 	require.ErrorContains(t, err, expectedErrPrefix)
+}
+
+// encodeManifest basically copies the details of kustomize-build-dirs
+// which takes in a path to a kustomize directory and writes the built
+// manifests to a file whose name is the directory path base64 encoded
+func encodeManifest(t *testing.T, manifestPath string) string {
+	t.Helper()
+
+	src, err := os.Open(manifestPath)
+	require.NoErrorf(t, err, "failed to open manifest path %s", manifestPath)
+
+	dstPath := filepath.Join(t.TempDir(), base64.StdEncoding.EncodeToString([]byte(manifestPath)))
+	dst, err := os.Create(dstPath)
+	require.NoError(t, err, "failed to create temp manifest")
+
+	_, err = io.Copy(dst, src)
+	require.NoError(t, err, "failed to copy manifest")
+
+	return dstPath
 }
 
 func TestFailsOnUnparseableManifest(t *testing.T) {
@@ -47,7 +80,8 @@ func TestFailsOnUnparseableManifest(t *testing.T) {
 	require.NoError(t, os.WriteFile(manifestPath, []byte(invalidManifest), 0o600))
 	expectedErrPrefix := "Failed reading manifests from " + manifestPath
 
-	err := validateOpsLevelAnnotationsForManifests([]string{manifestPath})
+	encodedPath := encodeManifest(t, manifestPath)
+	err := validateOpsLevelAnnotationsForManifests([]string{encodedPath})
 
 	require.ErrorContains(t, err, expectedErrPrefix)
 }
@@ -68,8 +102,9 @@ func TestFailsOnInvalidManifests(t *testing.T) {
 				require.NoErrorf(t, err, "Can't find output file for %s", entry.Name())
 				// .out files contain trailing newlines, but the error will not
 				expectedErr := strings.TrimSuffix(fmt.Sprintf("failed validating manifests from %s: %s", manifest, string(expectedOut)), "\n")
+				encodedPath := encodeManifest(t, manifest)
 
-				gotErr := validateOpsLevelAnnotationsForManifests([]string{manifest})
+				gotErr := validateOpsLevelAnnotationsForManifests([]string{encodedPath})
 				require.Error(t, gotErr)
 				require.EqualError(t, gotErr, expectedErr)
 			})
@@ -85,7 +120,8 @@ func TestAcceptsValidManifests(t *testing.T) {
 	for _, entry := range entries {
 		manifest := filepath.Join(validManifestsDir, entry.Name())
 		t.Run(entry.Name(), func(t *testing.T) {
-			require.NoError(t, validateOpsLevelAnnotationsForManifests([]string{manifest}))
+			encodedPath := encodeManifest(t, manifest)
+			require.NoError(t, validateOpsLevelAnnotationsForManifests([]string{encodedPath}))
 		})
 	}
 }
